c4b7cf9c999d5989f9698bb3e9f7c45aad267ae5
Enu Azubike <enuazubike88@gmail.com>
2021-09-18 03:30:57 +0100

    created test for createUser endpoint

--- src/test/java/com/azubike/ellipsis/mobile/app/ws/rest_assured_test/TestCreateUser.java
+++ src/test/java/com/azubike/ellipsis/mobile/app/ws/rest_assured_test/TestCreateUser.java
@@ -1,19 +1,55 @@
 package com.azubike.ellipsis.mobile.app.ws.rest_assured_test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 class TestCreateUser {
+	public final String CONTEXT_PATH = "/mobile-app-ws";
 
 	@BeforeEach
 	void setUp() throws Exception {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
 	}
 
 	@Test
 	void testCreateUser() {
-		fail("Not yet implemented");
+
+		List<Map<String, Object>> userAddresses = new ArrayList<>();
+
+		Map<String, Object> shippingAddresses = new HashMap<>();
+		shippingAddresses.put("city", "Vancover");
+		shippingAddresses.put("country", "Canada");
+		shippingAddresses.put("streetName", "Dancing ave");
+		shippingAddresses.put("postalCode", "12345");
+		shippingAddresses.put("type", "shipping");
+		userAddresses.add(shippingAddresses);
+
+		Map<String, Object> userDetails = new HashMap<>();
+		userDetails.put("firstName", "Azubike");
+		userDetails.put("lastName", "Enu");
+		userDetails.put("email", "enuazubike88@gmail.com");
+		userDetails.put("password", "12345");
+		userDetails.put("addresses", userAddresses);
+
+		Response response = given().contentType("application/json").accept("application/json").body(userDetails).when()
+				.post(CONTEXT_PATH + "/users").then().statusCode(200).contentType("application/json").extract()
+				.response();
+
+		assertNotNull(response);
+		String userId = response.jsonPath().getString("userId");
+		assertNotNull(userId);
 	}
 
 }
